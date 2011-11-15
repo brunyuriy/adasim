2023974488fc8a2668c60459e26e98e49fe854bf
ramaswamyj12 <ramaswamyj12@localhost>
2011-11-15 09:24:40 -0800

    CarInfo: Added comments

--- src/traffic/model/CarInfo.java
+++ src/traffic/model/CarInfo.java
@@ -1,9 +1,15 @@
+/**
+ * Jonathan Ramaswamy
+ * CarInfo
+ * CarInfo holds all the information for a car object, including its starting, ending,
+ * and current positions. This info object can be read by other cars and used in conjunction
+ * with a strategy algorithm to make decisions
+ */
 package traffic.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import traffic.strategy.CarStrategy;
 import traffic.strategy.NoiseStrategy;
 import traffic.strategy.RandomNoiseStrategy;
 
@@ -29,58 +35,80 @@ public class CarInfo {
 		noise = new RandomNoiseStrategy();
 	}
 	
+	/**
+	 * @return The starting node for the car
+	 */
 	public int getStartNode() {
 		return start;
 	}
 	
-	public void setStartNode(int s) {
-		start = s;
-	}
-	
+	/**
+	 * @return The ending node for the car
+	 */
 	public int getEndNode() {
 		return end;
 	}
 	
-	public void setEndNode(int e) {
-		end = e;
-	}
-	
+	/**
+	 * @return The current node of the car
+	 */
 	public int getCurrentPosition() {
 		return current;
 	}
 	
+	/**
+	 * Sets the current position to the given variable c
+	 */
 	public void setCurrentPosition(int c) {
 		current = c;
 	}
 	
+	/**
+	 * @return The number of this car
+	 */
 	public int getCarNum() {
 		return carNum;
 	}
 	
-	public void setCarNum(int cn) {
-		carNum = cn;
-	}
-	
+	/**
+	 * @return The turn delay at this node
+	 */
 	public int getDelay() {
 		return delay;
 	}
 	
-	public void setDelay(int l) {
-		delay = l;
+	/**
+	 * Sets the turn delay to the given variable d
+	 */
+	public void setDelay(int d) {
+		delay = d;
 	}
 	
+	/**
+	 * @return True if the car is at its ending node, false otherwise
+	 */
 	public boolean atDestination() {
 		return finish;
 	}
 	
+	/**
+	 * Sets the finish variable to true if the car's current position
+	 * is the same as its ending position
+	 */
 	public void setFinish() {
 		finish = current == end;
 	}
 	
+	/**
+	 * @return The path the car will take from start to finish
+	 */
 	public List<Integer> getPath() {
 		return path;
 	}
 	
+	/**
+	 * Sets the path for the car to the given list p
+	 */
 	public void setPath(List<Integer> p) {
 		path = p;
 	}
