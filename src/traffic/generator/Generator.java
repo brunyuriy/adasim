e522b4509ed1c879903149837bb26af188d00c19
wuttkej <wuttkej@gmail.com>
2011-12-03 14:25:14 -0800

    Started implementing the random generator.

---
+++ src/traffic/generator/Generator.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2011 - Jochen Wuttke.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jochen Wuttke (wuttkej@gmail.com) - initial API and implementation
+ ********************************************************************************
+ *
+ * Created: Dec 3, 2011
+ */
+
+package traffic.generator;
+
+/**
+ * @author Jochen Wuttke - wuttkej@gmail.com
+ *
+ */
+public class Generator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
