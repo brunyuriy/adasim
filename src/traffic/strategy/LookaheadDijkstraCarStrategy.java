6c586133e15835e8ee825532e8b885e0418445e2
ramaswamyj12 <ramaswamyj12@localhost>
2011-12-04 13:47:21 -0800

    Added incomplete LookaheadDijkstraCarStrategy class so testing program would compile

---
+++ src/traffic/strategy/LookaheadDijkstraCarStrategy.java
@@ -0,0 +1,19 @@
+package traffic.strategy;
+
+import java.util.List;
+
+import traffic.graph.Graph;
+
+public class LookaheadDijkstraCarStrategy implements CarStrategy{
+	
+	public LookaheadDijkstraCarStrategy(int i) {
+		
+	}
+
+	@Override
+	public List<Integer> getPath(Graph g, int c, int d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
