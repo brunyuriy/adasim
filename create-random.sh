6fb5b5a8da10eb657f7755d935353b989b319197
wuttkej <wuttkej@gmail.com>
2011-12-05 14:40:14 -0800

    Added a simple script that runs the random generator and then runs the simulation on the generated file.

---
+++ create-random.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#Script:
+#Purpose:
+#Project:
+#Author: Jochen Wuttke	jochen.wuttke@gmx.de
+#Date:
+#Version:
+#
+
+CP=lib/jdom-1.1.2.jar:lib/jopt-simple-3.2.jar:lib/log4j-1.2.16.jar:bin:
+
+java -cp $CP traffic.generator.Generator -N 1000 -C 500 -D 4 -o random.xml -d 3:6 \
+-S traffic.strategy.DijkstraCarStrategy
+
+java -cp $CP traffic.TrafficMain -I random.xml 
+
