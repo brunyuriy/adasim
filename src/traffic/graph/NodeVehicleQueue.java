bba3ab2c0a8c17fcf5c6c45983b2f7d6f8357f41
wuttkej <wuttkej@gmail.com>
2011-12-13 11:14:31 -0800

    Implemented abstraction to handle the queue of cars on each node

---
+++ src/traffic/graph/NodeVehicleQueue.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2011 - Jochen Wuttke.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jochen Wuttke (wuttkej@gmail.com) - initial API and implementation
+ ********************************************************************************
+ *
+ * Created: Dec 13, 2011
+ */
+
+package traffic.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import traffic.model.Car;
+
+/**
+ * @author Jochen Wuttke - wuttkej@gmail.com
+ *
+ */
+final class NodeVehicleQueue {
+	
+	private final static int PARKED = -1;
+	private Map<Integer, Set<Car>> queue;
+	
+	/**
+	 * 
+	 */
+	public NodeVehicleQueue() {
+		queue = new HashMap<Integer, Set<Car>>();
+		queue.put( PARKED, new HashSet<Car>() );
+		queue.put( 0, new HashSet<Car>() );
+	}
+	
+	void enqueue( Car c, int delay ) {
+		assert delay >= 0;
+		ensureBucketExists( delay );
+		queue.get( delay ).add(c);
+	}
+
+	/**
+	 * @param delay
+	 */
+	private void ensureBucketExists(int delay) {
+		if ( queue.get(delay) == null ) {
+			queue.put(delay, new HashSet<Car>() );
+		}
+	}
+
+	Set<Car> moveCars() {
+		Set<Car> fs = queue.remove(0);
+		SortedSet<Integer> keys = new TreeSet<Integer>( queue.keySet() );
+		for ( Integer key : keys ) {
+			if ( key == PARKED ) continue;	//default cases handled separately
+			Set<Car> c = queue.remove(key);
+			queue.put(key-1, c );
+		}
+		return fs;
+	}
+	
+	void park( Car c ) {
+		removeFromQueue( c );
+		queue.get( PARKED ).add( c );
+	}
+
+	/**
+	 * @param c
+	 */
+	private void removeFromQueue(Car c) {
+		for ( Integer key : queue.keySet() ) {
+			if ( key == PARKED ) continue;
+			if ( queue.get(key).remove(c) ) return;	//short circuit
+		}
+	}
+	
+	/**
+	 * @return <code>true</code> if there are no cars in the queue that are waiting to move
+	 */
+	boolean isEmpty() {
+		for ( Integer key : queue.keySet() ) {
+			if ( key == PARKED ) continue;
+			if ( !queue.get(key).isEmpty() ) return false; //short circuit
+		}
+		return true;
+	}
+}
---
+++ test/traffic/graph/NodeVehicleQueueTest.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2011 - Jochen Wuttke.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jochen Wuttke (wuttkej@gmail.com) - initial API and implementation
+ ********************************************************************************
+ *
+ * Created: Dec 13, 2011
+ */
+
+package traffic.graph;
+
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import traffic.model.Car;
+
+
+/**
+ * @author Jochen Wuttke - wuttkej@gmail.com
+ *
+ */
+public class NodeVehicleQueueTest {
+	
+	private NodeVehicleQueue queue;
+	
+	@Before
+	public void setUp() {
+		queue = new NodeVehicleQueue();
+	}
+	
+	@Test
+	public void parkNonContainedCar() {
+		assertTrue( queue.isEmpty() );
+		queue.park( new Car(2, 3, null, 99) );
+		assertTrue( queue.isEmpty() );		
+	}
+	
+	@Test
+	public void parkContainedCar() {
+		Car c = new Car(2, 3, null, 99);
+		queue.enqueue(c, 4);
+		assertFalse(queue.isEmpty());
+		queue.park(c);
+		assertTrue(queue.isEmpty());
+	}
+	
+	@Test
+	public void moveMultipleDelays() {
+		Car c0 = new Car(2, 3, null, 0);
+		Car c1 = new Car(2, 3, null, 1);
+		Car c3 = new Car(2, 3, null, 3);
+		Car c33 = new Car(2, 3, null, 3);
+		Car c5 = new Car(2, 3, null, 5);
+		queue.enqueue(c0, 0);
+		queue.enqueue(c1, 1);
+		queue.enqueue(c3, 3);
+		queue.enqueue(c33, 3);
+		queue.enqueue(c5, 5);
+		assertFalse(queue.isEmpty());
+		Set<Car> s = queue.moveCars();
+		assertEquals(1, s.size());
+		assertEquals( c0, s.iterator().next() );
+		s = queue.moveCars();
+		assertEquals(1, s.size());
+		s = queue.moveCars();
+		assertNull( "No cars expected.", s );
+		s = queue.moveCars();
+		assertEquals( 2, s.size() );
+		s = queue.moveCars();
+		assertNull( s );
+		s = queue.moveCars();
+		assertEquals(1, s.size());
+		assertTrue( queue.isEmpty() );
+	}
+
+}
