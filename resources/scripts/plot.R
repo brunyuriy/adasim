f5215c47339f55f1063f1689e8b81f7452e761de
Jochen <jochen.wuttke@gmx.de>
2011-12-16 21:33:35 -0800

    added more cool stuff

--- resources/scripts/plot.R
+++ resources/scripts/plot.R
@@ -8,8 +8,18 @@ map_s <- function(str) {
 }
 
 plot_data <- function(file) {
-	data <- read.table( file, header=T, sep=',')
+	data <- read.table( paste(file, "csv", sep="."), header=T, sep=',')
 	p <- tapply( data$Time, list(data$Hops, data$Strategy) , mean)
 	l <- levels(data$Strategy)
+	pdf( paste(file, "pdf", sep=".") )
 	barplot(t(p), beside=T, legend= c( map_s(l[1]) , map_s(l[2]) ))
+	dev.off()
+}
+
+plot_all_data <- function() {
+	files <- list.files( path=".", pattern=".+.csv$" )
+	for ( file in files) {
+		s <- strsplit( file, ".csv" )
+		plot_data(s)
+	}
 }
