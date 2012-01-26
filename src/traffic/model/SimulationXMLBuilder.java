21ff20b18893ee61747f7ae7c453477e46805845
wuttkej <wuttkej@gmail.com>
2012-01-26 15:43:44 -0800

    Removed code duplication from the Generator and made it work with the new SimulationXMLBuilder.

--- src/traffic/model/SimulationXMLBuilder.java
+++ src/traffic/model/SimulationXMLBuilder.java
@@ -256,7 +256,7 @@ public class SimulationXMLBuilder {
 	 * @param node
 	 */
 	private void buildNeigbors(List<GraphNode> nodes, Element node) {
-		String[] neighbors = node.getAttributeValue("neighbors").split(" ");
+		String[] neighbors = node.getAttributeValue("neighbors").trim().split(" ");
 		GraphNode gn = getNode( nodes, node );
 		for ( String n : neighbors ) {
 			int nn = Integer.parseInt(n);
