6b47b1bd8a99b4c47e82b81aff4b9edd0dd3f24a
wuttkej <wuttkej@gmail.com>
2012-03-08 14:30:06 -0800

    Fixed the schema loading for the third time.

--- src/traffic/model/SimulationXMLReader.java
+++ src/traffic/model/SimulationXMLReader.java
@@ -64,8 +64,8 @@ final public class SimulationXMLReader {
 		SAXBuilder sbuilder = new SAXBuilder(true);
 		sbuilder.setProperty("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema");
 		try {
-			//URL res = SimulationXMLReader.class.getClassLoader().getResource("resources/xml/adasim.xsd");
-			sbuilder.setProperty("http://java.sun.com/xml/jaxp/properties/schemaSource", "/Users/Jonathan/workspace/TrafficSim/resources/xml/adasim.xsd" );
+			URL res = SimulationXMLReader.class.getClassLoader().getResource("resources/xml/adasim.xsd");
+			sbuilder.setProperty("http://java.sun.com/xml/jaxp/properties/schemaSource", res.openStream() );
 			sbuilder.setErrorHandler(new SimpleErrorHandler());
 			doc = sbuilder.build(f);
 		} catch ( FileNotFoundException e) {
