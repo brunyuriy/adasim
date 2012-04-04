b57ddd09e287b8da948714886e232bc35bf7272d
wuttkej <wuttkej@gmail.com>
2012-04-04 14:23:31 -0700

    Updated VERSION to full target milestone 1.0.0

--- test/traffic/VersionTest.java
+++ test/traffic/VersionTest.java
@@ -41,7 +41,7 @@ public class VersionTest {
 
 	@Test
 	public void test() {
-		assertEquals( Version.versionString(), "Adasim - Version 0.5.2" );
+		assertEquals( Version.versionString(), "Adasim - Version 1.0.0" );
 	}
 
 }
