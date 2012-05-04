9e3a37db0e7e95f884c3b7d3d9d4cd67b5b42e01
wuttkej <wuttkej@gmail.com>
2012-05-04 11:37:23 -0700

    Incomplete - task : Issue 74 - Add filters to vehicles Added reflective calls to hidden property methods.

---
+++ src/adasim/util/ReflectionUtils.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (C) 2011 - 2012 Jochen Wuttke
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+
+ *
+ * Contributors:
+ *    Jochen Wuttke (wuttkej@gmail.com) - initial API and implementation
+ ********************************************************************************
+ *
+ * Created: May 4, 2012
+ */
+
+package adasim.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import adasim.model.RoadSegment;
+
+/**
+ * 
+ * @throws NoSuchMethodException if the passed in object does not declare the 
+ * property method specified
+ * @throws ReflectionException for any other type of exception caused by 
+ * reflection. The original exception is nested for inspection.
+ * 
+ * @author Jochen Wuttke - wuttkej@gmail.com
+ *
+ */
+public class ReflectionUtils {
+	public static int getIntProperty( RoadSegment s, String property ) throws NoSuchMethodException, ReflectionException {
+		try {
+			Method m = s.getClass().getDeclaredMethod( property, new Class<?>[0] );
+			m.setAccessible(true);
+			return (Integer) m.invoke(s, new Object[0] );
+		} catch (NoSuchMethodException e) {
+			throw e;
+		} catch ( Exception e ) {
+			throw new ReflectionException( e );
+		}
+	}
+}
